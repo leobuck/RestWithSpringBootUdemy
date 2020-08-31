3c1fc6e586d87520ab2c2d07ea7c20dca2bb9414
Leo Buck <leobuck12@gmail.com>
2020-08-31 13:42:32 -0300

    Migrations com o Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (address, first_name, gender, last_name) 
+VALUES 
+('São Paulo - Brasil', 'Leonardo', 'Male', 'Buck de Godoy'),
+('Minas Gerais - Brasil', 'Pedro', 'Male', 'Paulo'),
+('Minas Gerais - Brasil', 'Marcos', 'Male', 'Oliveira');
