f66267f04bd75d18163cfe38cfdc080ae5b428f1
leobuck <leobuck12@gmail.com>
2020-09-01 12:43:44 -0300

    Content Negociation via QUERY Parameter

--- src/main/java/br/com/leo/config/WebConfig.java
+++ src/main/java/br/com/leo/config/WebConfig.java
@@ -12,11 +12,20 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//		configurer.favorParameter(false)
+//			.ignoreAcceptHeader(false)
+//			.defaultContentType(MediaType.APPLICATION_JSON)
+//			.mediaType("json", MediaType.APPLICATION_JSON)
+//			.mediaType("xml", MediaType.APPLICATION_XML);
+		
 		configurer.favorParameter(false)
-			.ignoreAcceptHeader(false)
-			.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);
+		.favorParameter(true)
+		.parameterName("mediaType")
+		.ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 	
 }
